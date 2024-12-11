35dc477d4c76e55408fb23dd49201ef08d8f4ccf
HOLLMW <maxwholloway@gmail.com>
2024-12-11 16:53:56 +0000

    views table

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,7 +6,13 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index(){
-        
+    public function index()
+    {
+        $posts = Post::with(['comments.user', 'user']) -> get();
+
+        return view('home', [
+            'posts' => $posts,
+            'isAdmin' => auth()->check() && auth()->user()->role === 'admin',
+        ]);
     }
 }
