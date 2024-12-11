9c5cf16776563173605113dfd0ff35a5f75138b7
HOLLMW <maxwholloway@gmail.com>
2024-12-11 16:41:30 +0000

    admin for user changes (middleware not registered)

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'role' => 'user', //default to user
         ];
     }
 
@@ -39,6 +40,14 @@ class UserFactory extends Factory
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
+        ]);
+    }
+
+
+    public function admin()
+    {
+        return $this->state([
+            'role' => 'admin',
         ]);
     }
 }
